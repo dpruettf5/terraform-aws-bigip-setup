2ee011c5e77f072f771a8fd6b398c24fdb172ec3
Mark J Menger <mark.j.menger@gmail.com>
2022-01-06 17:07:15 +0000

    clean up

--- dotenv.tf
+++ dotenv.tf
@@ -1,4 +1,11 @@
+variable create_dotenv {
+  default     = false
+  type        = bool
+  description = "true/false flag indicating whether to create a .env file environment variable exports"
+}
+
 resource "local_file" "dotenv" {
+  count = var.create_dotenv ? 1 : 0
   content  = <<EOF
 bigip1=${module.bigip.mgmt_public_ips[0]}
 bigip2=${module.bigip.mgmt_public_ips[1]}
